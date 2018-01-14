82afb36ae7f18bc5618982d54d9f67c7f95830a9
Somov Alex <hitenok1@gmail.com>
2018-01-14 23:39:20 +0700

    change logic

--- src/ValidatorAlgebraicEquation.php
+++ src/ValidatorAlgebraicEquation.php
@@ -6,26 +6,20 @@ use InvalidArgumentException;
 
 class ValidatorAlgebraicEquation
 {
-    private $equation;
-
-    public function __construct(string $equation)
+    public function isCorrectnessParentheses(string $equation)
     {
         $this->checkCorrectSymbols($equation);
-        $this->equation = $equation;
-    }
-
-    public function isCorrectnessParentheses()
-    {
-        if (!$this->checkQuantityParentheses()) {
+        
+        if (!$this->checkQuantityParentheses($equation)) {
             return false;
         }
 
-        $length = strlen($this->equation);
+        $length = strlen($equation);
         $numberOpeningParenthesis = 0;
         for ($i = 0; $i < $length; $i++) {
-            if ($this->equation[$i] == '(') {
+            if ($equation[$i] == '(') {
                 $numberOpeningParenthesis++;
-            } elseif ($this->equation[$i] == ')') {
+            } elseif ($equation[$i] == ')') {
                 $numberOpeningParenthesis--;
             } else {
                 continue;
@@ -39,9 +33,9 @@ class ValidatorAlgebraicEquation
         return true;
     }
 
-    protected function checkQuantityParentheses()
+    protected function checkQuantityParentheses($equation)
     {
-        return substr_count($this->equation, '(') == substr_count($this->equation, ')');
+        return substr_count($equation, '(') == substr_count($equation, ')');
     }
 
     protected function checkCorrectSymbols($equation)
